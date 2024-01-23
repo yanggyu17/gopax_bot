05552df3679afb99d0b121010ac2c663000b6f94
yanggyu <youngguy131@gmail.com>
2024-01-23 11:11:06 +0900

    upload bot.py

---
+++ bot.py
@@ -0,0 +1,53 @@
+import time, base64, hmac, hashlib, requests, json
+
+file_path = "api_key.txt"
+
+## load api key
+with open(file_path, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    conkey = lines[0].strip()
+    secretkey = lines[1].strip()
+    
+# 발급받은 api키와 시크릿키를 입력한다
+apikey = conkey
+secret = secretkey
+
+asset_name = "BTC"
+
+# nonce값 생성
+nonce = str(time.time())
+method = 'GET'
+request_path = f'/balances/{asset_name}'
+
+#필수 정보를 연결하여 prehash 문자열을 생성함
+what = nonce + method + request_path
+#base64로 secret을 디코딩함
+key = base64.b64decode(secret)
+#hmac으로 필수 메시지에 서명하고
+signature = hmac.new(key, str(what).encode('utf-8'), hashlib.sha512)
+#그 결과물을 base64로 인코딩함
+signature_b64 = base64.b64encode(signature.digest())
+
+# HTML 소스 가져오기
+def HTMLsouceGet(p):
+	print (p.text)
+	
+custom_headers = {
+	'API-Key': apikey,
+	'Signature': signature_b64,
+	'Nonce': nonce
+}
+
+def main():
+	# method = get
+	req = requests.get(url = 'https://api.gopax.co.kr' + request_path, headers = custom_headers)
+
+	if req.ok:
+		HTMLsouceGet(req)
+
+	else:
+		print ('요청 에러')
+		HTMLsouceGet(req)
+
+if __name__ == '__main__':
+	main()
